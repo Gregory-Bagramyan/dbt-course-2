4dca7dfd669f67271ad7b8762a28e0b6d68b81f9
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2025-11-29 17:52:13 +0100

    fixed logic

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -11,5 +11,5 @@ SELECT * FROM src_reviews
 WHERE review_text is not null
 
 {% if is_incremental() %}
-  where review_date > (select max(review_date) from {{ this }})
+  AND review_date > (select max(review_date) from {{ this }})
 {% endif %}
