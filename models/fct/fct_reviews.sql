98544b999aea6b45515350b59b2452acee99e0a7
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2025-11-29 16:50:01 +0100

    updated incremental logic

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -11,5 +11,5 @@ SELECT * FROM src_reviews
 WHERE review_text is not null
 
 {% if is_incremental() %}
-  AND review_date > (select max(review_date) from {{ this }})
+  where review_date > (select max(review_date) from {{ this }})
 {% endif %}
